e18226a3a9065fc655fd368e845b7195027681c7
curder <curder@users.noreply.github.com>
2022-01-15 14:54:44 +0000

    deploy: 8363aa2867d5283d4f6bf857bd0cedf5faf7060e

--- assets/how-to-use-properties.html.8d1af66a.js
+++
@@ -1,1 +0,0 @@
-const e={key:"v-657436c6",path:"/languages/laravel/livewire/how-to-use-properties.html",title:"Livewire \u5C5E\u6027",lang:"zh-CN",frontmatter:{},excerpt:"",headers:[],git:{updatedTime:1642257842e3,contributors:[{name:"curder",email:"q.curder@gmail.com",commits:1}]},filePathRelative:"languages/laravel/livewire/how-to-use-properties.md"};export{e as data};
