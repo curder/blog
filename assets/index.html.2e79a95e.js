d4e145a48fac4cfb4ed2ed816ca8cc36119b2e3d
curder <curder@users.noreply.github.com>
2022-10-27 08:45:37 +0000

    deploy: fdb19bf5a94b785de615f035457b7a079cd677aa

---
+++ assets/index.html.2e79a95e.js
@@ -0,0 +1,1 @@
+const e={key:"v-2b91000c",path:"/languages/vue/vee-validate/",title:"Vee Validate",lang:"zh-CN",frontmatter:{},excerpt:"",headers:[],git:{updatedTime:1666860281e3,contributors:[{name:"curder",email:"q.curder@gmail.com",commits:1}]},filePathRelative:"languages/vue/vee-validate/README.md"};export{e as data};
